e9e10766c28505ff008eaf988625568477763f7b
Piotr Romanowski <piotr@romanowski.me>
2021-10-24 21:36:12 +0200

    Delete unused crypto extensions

--- Vulcanova.Uonet/Crypto/CryptoExtensions.cs
+++ Vulcanova.Uonet/Crypto/CryptoExtensions.cs
@@ -1,8 +1,5 @@
-using System;
 using System.IO;
-using Org.BouncyCastle.Security;
 using Org.BouncyCastle.Utilities.IO.Pem;
-using Org.BouncyCastle.X509;
 using PemWriter = Org.BouncyCastle.OpenSsl.PemWriter;
 
 namespace Vulcanova.Uonet.Crypto
@@ -13,11 +10,6 @@ namespace Vulcanova.Uonet.Crypto
         {
             return DumpToString((object) @object);
         }
-        
-        public static string DumpToString(this X509Certificate cert)
-        {
-            return DumpToString((object) cert);
-        }
 
         private static string DumpToString(object o)
         {
@@ -26,12 +18,6 @@ namespace Vulcanova.Uonet.Crypto
             pemWriter.WriteObject(o);
 
             return textWriter.ToString();
-        }
-
-        public static string GetFingerprint(this X509Certificate x509Certificate)
-        {
-            var digest = DigestUtilities.CalculateDigest("sha1", x509Certificate.GetEncoded());
-            return BitConverter.ToString(digest).Replace("-", "").ToLower();
         }
     }
 }
