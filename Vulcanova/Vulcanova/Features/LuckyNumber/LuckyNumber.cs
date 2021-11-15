0ced617e6e7833b0c78a479ad217f0382a5c4b0c
Piotr Romanowski <piotr@romanowski.me>
2021-11-15 17:57:52 +0100

    Remove redundant LuckyNumber _id field

--- Vulcanova/Vulcanova/Features/LuckyNumber/LuckyNumber.cs
+++ Vulcanova/Vulcanova/Features/LuckyNumber/LuckyNumber.cs
@@ -4,7 +4,6 @@ namespace Vulcanova.Features.LuckyNumber
 {
     public class LuckyNumber
     {
-        private int _id;
         public int AccountId { get; set; }
         public int ConstituentId { get; set; }
         public DateTime Date { get; set; }
