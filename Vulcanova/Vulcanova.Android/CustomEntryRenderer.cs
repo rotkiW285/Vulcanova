cf91af3aafc8c2e2d4bc304318e86710cc18bb24
Dawid <66019090+ASSYNU@users.noreply.github.com>
2022-05-21 19:54:10 +0200

    Dark mode manual add account fix - #3 and #31 issues fix (#34)
    * Dark mode manual add account fix
    
    * Underline color dynamic change
    
    * Hot fix

---
+++ Vulcanova/Vulcanova.Android/CustomEntryRenderer.cs
@@ -0,0 +1,35 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using Vulcanova.Android;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace Vulcanova.Android 
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> element)
+        {
+            base.OnElementChanged(element);
+
+            if (Control == null || element.NewElement == null) return;
+
+            Control.BackgroundTintList = ColorStateList.ValueOf(Element.TextColor.ToAndroid());
+        }
+
+        protected override void OnElementPropertyChanged(object sender,
+            System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName != VisualElement.IsFocusedProperty.PropertyName) return;
+
+            Control.BackgroundTintList = ColorStateList.ValueOf(Element.IsFocused
+                ? Resources!.GetColor(Resource.Color.colorAccent, Context!.Theme)
+                : Element.TextColor.ToAndroid());
+        }
+    }
+}
