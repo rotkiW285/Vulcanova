b28afa58eae5077e41ca6e6170fd8937d9db1e47
Piotr Romanowski <piotr@romanowski.me>
2022-09-05 20:51:51 +0200

    Pass default text color to GradeColorConverter

--- Vulcanova/Vulcanova/Features/Grades/GradeColorConverter.cs
+++ Vulcanova/Vulcanova/Features/Grades/GradeColorConverter.cs
@@ -12,19 +12,7 @@ public class GradeColorConverter : IValueConverter
 
         if (numericValue == 0)
         {
-            var colorKey = Application.Current.RequestedTheme == OSAppTheme.Dark
-                ? "DarkPrimaryTextColor"
-                : "LightPrimaryTextColor";
-
-            // https://github.com/xamarin/Xamarin.Forms/issues/5976
-            if (Application.Current.Resources.TryGetValue(colorKey, out var color))
-            {
-                return color;
-            }
-
-            return Application.Current.RequestedTheme == OSAppTheme.Dark
-                ? Color.White
-                : Color.Black;
+            return parameter;
         }
 
         var bytes = BitConverter.GetBytes(numericValue);
