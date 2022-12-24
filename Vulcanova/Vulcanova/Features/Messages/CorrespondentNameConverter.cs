06bb92f99fba8f9d0719ba141159585461bac62e
Piotr Romanowski <piotr@romanowski.me>
2022-12-24 16:15:13 +0100

    Display receiver's name on sent messages

---
+++ Vulcanova/Vulcanova/Features/Messages/CorrespondentNameConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Globalization;
+using System.Linq;
+using Vulcanova.Uonet.Api.MessageBox;
+using Xamarin.Forms;
+
+namespace Vulcanova.Features.Messages;
+
+public class CorrespondentNameConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => value switch
+        {
+            Message { Folder: MessageBoxFolder.Sent } m => string.Join(", ", m.Receiver.Select(r => r.Name)),
+            Message m => m.Sender.Name,
+            { } => throw new ArgumentException($"Unsupported argument of type {value.GetType()}",
+                nameof(value)),
+            null => string.Empty
+        };
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
