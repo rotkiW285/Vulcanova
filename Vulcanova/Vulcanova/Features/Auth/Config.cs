bd2d3f22bde7ca24110018b3180668672096ddd1
Piotr Romanowski <piotr@romanowski.me>
2023-01-06 14:13:08 +0100

    Sync account periods every 1 day

--- Vulcanova/Vulcanova/Features/Auth/Config.cs
+++ Vulcanova/Vulcanova/Features/Auth/Config.cs
@@ -20,6 +20,7 @@ public static class Config
         container.RegisterForNavigation<AccountPickerView>();
 
         container.RegisterSingleton<AccountAwarePageTitleViewModel>();
+        container.RegisterScoped<IAccountSyncService, AccountSyncService>();
 
         container.RegisterScoped<IAuthenticationService, AuthenticationService>();
     }
