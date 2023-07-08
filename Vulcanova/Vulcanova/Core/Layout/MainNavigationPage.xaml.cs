69d08d152b72639792a41c6c05fd2190383fda35
Piotr Romanowski <piotr@romanowski.me>
2023-07-08 11:56:48 +0200

    Throttle session errors

--- Vulcanova/Vulcanova/Core/Layout/MainNavigationPage.xaml.cs
+++ Vulcanova/Vulcanova/Core/Layout/MainNavigationPage.xaml.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Linq;
 using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using FFImageLoading.Svg.Forms;
+using ReactiveUI;
 using Rg.Plugins.Popup.Contracts;
 using Vulcanova.Core.Layout.Controls;
 using Vulcanova.Core.Rx;
@@ -14,15 +19,49 @@ namespace Vulcanova.Core.Layout;
 [XamlCompilation(XamlCompilationOptions.Compile)]
 public partial class MainNavigationPage
 {
+    private readonly Subject<Exception> _exceptions = new();
+
     public MainNavigationPage(IPopupNavigation navigationService)
     {
         InitializeComponent();
 
         Interactions.Errors.RegisterHandler(async ctx =>
         {
-            var (title, message, icon, action, command, parameter) = ctx.Input switch
+            await Task.Yield();
+            _exceptions.OnNext(ctx.Input);
+        });
+
+        var closes = _exceptions.Throttle(TimeSpan.FromSeconds(1));
+
+        closes
+            .Window(() => closes)
+            .SelectMany(w => w.ToList())
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .SelectMany(async exceptions =>
             {
-                VulcanException {StatusCode: 108} => (
+                static bool IsSessionError(Exception e) => e is VulcanException { StatusCode: 108 };
+
+                var sessionError = exceptions.FirstOrDefault(IsSessionError);
+                if (sessionError != null)
+                {
+                    await HandleException(sessionError);
+                }
+
+                foreach (var exception in exceptions.Where(e => !IsSessionError(e)))
+                {
+                    await HandleException(exception);
+                }
+
+                return Unit.Default;
+            })
+            .Subscribe();
+        return;
+
+        async Task HandleException(Exception exception)
+        {
+            var (title, message, icon, action, command, parameter) = exception switch
+            {
+                VulcanException { StatusCode: 108 } => (
                     Strings.SessionErrorPopupTitle,
                     Strings.SessionErrorAlertMessage,
                     SvgImageSource.FromResource("Vulcanova.Resources.Icons.alert-circle-outline.svg"),
@@ -36,15 +75,15 @@ public partial class MainNavigationPage
                     SvgImageSource.FromResource("Vulcanova.Resources.Icons.alert-circle-outline.svg"),
                     Strings.ShowErrorDetails,
                     ViewModel.ShowErrorDetails,
-                    ctx.Input),
+                    exception),
 
                 _ => (
                     Strings.ErrorAlertTitle,
-                    ctx.Input.Message,
+                    exception.Message,
                     SvgImageSource.FromResource("Vulcanova.Resources.Icons.close-circle.svg"),
                     Strings.ShowErrorDetails,
-                    (ICommand) ViewModel.ShowErrorDetails,
-                    (object) ctx.Input)
+                    (ICommand)ViewModel.ShowErrorDetails,
+                    (object)exception)
             };
 
             var popup = new SnackPopup(title, message, icon, action, command, parameter);
@@ -54,6 +93,6 @@ public partial class MainNavigationPage
             await Task.Delay(5000);
 
             await navigationService.RemovePageAsync(popup);
-        });
+        }
     }
 }
