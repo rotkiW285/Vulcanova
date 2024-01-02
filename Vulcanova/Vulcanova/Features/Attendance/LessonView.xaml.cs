ea89a293cbd0dd0a766835c408c7b46500d19496
Piotr Romanowski <piotr@romanowski.me>
2024-01-02 12:07:12 +0100

    Don't run justification mode animation on first LessonView render if not needed

--- Vulcanova/Vulcanova/Features/Attendance/LessonView.xaml.cs
+++ Vulcanova/Vulcanova/Features/Attendance/LessonView.xaml.cs
@@ -40,12 +40,31 @@ public partial class LessonView
     {
         InitializeComponent();
 
-        var showCol = new Animation(v => Col1.Width = v, 0, 40, Easing.Linear);
-        var hideCol = new Animation(v => Col1.Width = v, 40, 0, Easing.Linear);
+        const int targetAnimWidth = 40;
+
+        var showCol = new Animation(v => Col1.Width = v, 0, targetAnimWidth, Easing.Linear);
+        var hideCol = new Animation(v => Col1.Width = v, targetAnimWidth, 0, Easing.Linear);
 
         this.WhenAnyValue(v => v.JustificationMode, v => v.Lesson)
             .Where(o => o.Item2 != null)
             .Select(o => o.Item1 && o.Item2.CanBeJustified)
+            .Select(o => (bool?)o)
+            .StartWith((bool?) null)
+            .Buffer(2, 1)
+            .Where(x => x.Count > 1)
+            .Where(x =>
+            {
+                var prev = x[0];
+                var curr = x[1]!.Value;
+
+                if (prev == null && curr)
+                {
+                    return true;
+                }
+
+                return prev.HasValue && prev != curr;
+            })
+            .Select(v => v[1]!.Value)
             .Subscribe(v =>
             {
                 this.AbortAnimation("GridColWidthAnim");
